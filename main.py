5a1bc31fc09ac65d5ccecdf7cc7e48996b7957f3
Enes Kurt <131592466+knockinonheavensdoor@users.noreply.github.com>
2023-05-18 21:45:25 +0300

    Update main.py
    version 1.1

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ clock = pygame.time.Clock()
 dt = 0
 pygame.init()
 genislik, yukseklik = 800, 600
+buton_genislik = 200
+buton_yukseklik = 50
+buton_x = (genislik - buton_genislik) // 2
+buton_y = (yukseklik - buton_yukseklik) // 2
 screen = pygame.display.set_mode((genislik, yukseklik))
 pygame.display.set_caption("Ok Avcısı")
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -15,12 +19,14 @@ fonth = pygame.font.Font(None, 30)
 fontg = pygame.font.Font(None, 120)
 color = "white"
 Frame = True
-cnt = True
+cnt = False
 obj = []
 ok = []
 skor = 0
 fps = 0
 hiz = 300
+yuksekskor = 0
+
 sutun_yon = [[175, "yukari", 0], [325, "sol", 0], [475, "sag", 0], [625, "asagi", 0]]
 
 
@@ -69,9 +75,16 @@ class Ok:
 
 
 while Frame:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for olay in pygame.event.get():
+        if olay.type == pygame.QUIT:
             Frame = False
+        elif olay.type == pygame.MOUSEBUTTONDOWN:
+            fare_x, fare_y = pygame.mouse.get_pos()
+            if buton_x <= fare_x <= buton_x + buton_genislik and buton_y <= fare_y <= buton_y + buton_yukseklik:
+                print(obj)
+                skor = 0
+                hiz = 300
+                cnt = True
     if cnt:
         screen.fill("white")
         skoryazi = fonth.render(f"Skor: {skor} Hiz: {hiz} Sıklık (o/Ft): 1 / {70 - (hiz // 50)}", True, "red")
@@ -122,12 +135,25 @@ while Frame:
             i.konum.y += hiz * dt
             i.ciz()
             if i.konum.y > screen.get_height():
+                if skor > yuksekskor:
+                    yuksekskor = skor
+                obj = []
                 cnt = False
         pygame.display.flip()
     else:
-        screen.fill("black")
-        go = fontg.render("OYUN BITTI", True, "red")
-        screen.blit(go, (100, 100))
+        screen.fill("white")
+        pygame.draw.rect(screen, "yellow", (buton_x, buton_y, buton_genislik, buton_yukseklik))
+        okavcisi = fontg.render("Ok Avcısı", True, "red")
+        screen.blit(okavcisi, (215, 100))
+        font = pygame.font.Font(None, 32)
+        yuksekskoryazi = font.render(f"Yüksek Skor: {yuksekskor}", True, "red")
+        metin = font.render("Oyna", True, "red")
+        metin_yukseklik = metin.get_height()
+        metin_genislik = metin.get_width()
+        metin_x = buton_x + (buton_genislik - metin_genislik) // 2
+        metin_y = buton_y + (buton_yukseklik - metin_yukseklik) // 2
+        screen.blit(metin, (metin_x, metin_y))
+        screen.blit(yuksekskoryazi, (325,400))
     pygame.display.flip()
     dt = clock.tick(144) / 1000
 pygame.quit()
